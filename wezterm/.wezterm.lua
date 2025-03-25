8d0e45d5ccbed45158423a0a38a015c20bd4c524
Abhishek Amralkar <abhishekamralkar@gmail.com>
2025-03-24 20:31:10 -0700

    update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,14 +5,28 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
--- Spawn a fish shell in login mode
+-- Spawn a zsh shell in login mode
 config.default_prog = { '/usr/bin/zsh' }
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Bamboo Light'
-
+config.color_scheme = 'Batman'
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = true
+config.scrollback_lines = 100000
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 -- Font
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font('MesloLGS Nerd Font Mono')
+config.font_size = 12
+
+config.enable_tab_bar = false
+
+config.default_cursor_style = 'BlinkingBar'
+config.window_decorations = 'RESIZE'
 
 -- and finally, return the configuration to wezterm
 return config
